09fe4b6cee543601e31a8092497d6c48127455bc
Tomoloju Victor <tomoloju45@gmail.com>
2021-01-26 14:11:20 -1100

    init

---
+++ main/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "driver/gpio.h"
+#include "math.h"
+
+void app_main(void)
+{
+
+  /*
+write code here!!
+  */
+}
